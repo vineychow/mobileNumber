95505f81e7c255ec4e446635c2321344ccf1551b
hai.dev <hai.nan@ot24.com>
2012-07-24 14:13:26 +0800

    add valide method to check mobile number of China

---
+++ mobile/mobile.go
@@ -0,0 +1,14 @@
+package mobile
+
+import (
+	"regexp"
+)
+
+const (
+	regular = "^(13[0-9]|14[57]|15[0-35-9]|18[07-9])\\d{8}$"
+)
+
+func Validate(mobileNum string) bool {
+	reg := regexp.MustCompile(regular)
+	return reg.MatchString(mobileNum)
+}
